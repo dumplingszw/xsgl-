d3cfda2857b8cce5676d3180e76b3e627b5af4e7
dumplingszw <dumplingszw@users.noreply.github.com>
2026-06-12 23:47:26 +0800

    fix: import join us section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HeroSection from './components/HeroSection'
 import CategoryGrid from './components/CategoryGrid'
 import RecommendedArticles from './components/RecommendedArticles'
 import CampusShowcase from './components/CampusShowcase'
+import JoinUsSection from './components/JoinUsSection'
 import Footer from './components/Footer'
 import XiaokePet from './components/XiaokePet'
 
